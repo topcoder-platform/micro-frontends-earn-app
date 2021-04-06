c66d3a7695721f3ee40de6fd22eb2611a15d2dc3
Luiz R. Rodrigues <contato@luizrrodrigues.com.br>
2021-04-06 14:26:36 -0300

    Docker: Added missing --build-arg

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ APP_NAME=$1
 UPDATE_CACHE=""
 
 docker build -f docker/Dockerfile -t $APP_NAME:latest .
+--build-arg APPMODE=$APPMODE \
+--build-arg APPENV=$APPENV .
 
 docker create --name app $APP_NAME:latest
 
