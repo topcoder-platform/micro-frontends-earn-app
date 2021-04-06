5768bd6cd416709c207f5f57bec705cdbaffa1a1
Luiz R. Rodrigues <contato@luizrrodrigues.com.br>
2021-04-06 15:19:07 -0300

    Docker: Fix build command

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 APP_NAME=$1
 UPDATE_CACHE=""
 
-docker build -f docker/Dockerfile -t $APP_NAME:latest .
+docker build -f docker/Dockerfile -t $APP_NAME:latest \
 --build-arg APPMODE=$APPMODE \
 --build-arg APPENV=$APPENV .
 
