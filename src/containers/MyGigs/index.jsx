51045d630a47808bea48c88020e9a1f44dd3993c
dengjun <dengjun@huobi.com>
2021-06-21 13:45:11 +0800

    ci:disable profile update

--- src/containers/MyGigs/index.jsx
+++ src/containers/MyGigs/index.jsx
@@ -53,6 +53,7 @@ const MyGigs = ({
           <Button
             isPrimary
             size="lg"
+            disabled="true"
             onClick={() => {
               setOpenUpdateProfile(true);
             }}
