72b10517de23374adbf22bad4aab9794398e02f3
aT <atif.siddiqui2@topcoder.com>
2021-09-13 20:25:10 +0530

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   MFE_CONFIG: {
-    '@topcoder/micro-frontends-challenges-app': 'http://platform.topcoder.com/challenges-app/topcoder-micro-frontends-challenges-app.js',
-    '@topcoder/micro-frontends-gigs-app': 'http://platform.topcoder.com/gigs-app/topcoder-micro-frontends-gigs-app.js',
+    '@topcoder/micro-frontends-challenges-app': 'https://platform.topcoder.com/challenges-app/topcoder-micro-frontends-challenges-app.js',
+    '@topcoder/micro-frontends-gigs-app': 'https://platform.topcoder.com/gigs-app/topcoder-micro-frontends-gigs-app.js',
   }
 };
