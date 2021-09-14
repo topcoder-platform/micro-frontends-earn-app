e2e1006c9c0cb81bd4ce4c374572fdf27659306f
LieutenantRoger <gair_2015@hotmail.com>
2021-09-14 21:11:49 +0800

    add local-setup for earn-app

--- config/dev.js
+++ config/dev.js
@@ -1,2 +1,5 @@
 module.exports = {
+    MFE_CONFIG: {
+        '@topcoder/micro-frontends-another-app': 'http://localhost:8090/another-app/topcoder-micro-frontends-another-app.js',
+      }
 };
--- config/local-dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MFE_CONFIG: {
-    '@topcoder/micro-frontends-challenges-app': 'http://localhost:8009/challenges-app/topcoder-micro-frontends-challenges-app.js',
-    '@topcoder/micro-frontends-gigs-app': 'http://localhost:8010/gigs-app/topcoder-micro-frontends-gigs-app.js',
-  }
-};
