d254f073d6f6467c4260f82242534bdfbca6e2a9
LieutenantRoger <gair_2015@hotmail.com>
2021-10-02 10:34:46 +0800

    ci:redeploying

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,2 @@
 module.exports = {
-  MFE_CONFIG: {
-    "@topcoder/micro-frontends-challenges-app":
-      "https://platform.topcoder-dev.com/challenges-app/topcoder-micro-frontends-challenges-app.js",
-    "@topcoder/micro-frontends-gigs-app":
-      "http://localhost:8010/gigs-app/topcoder-micro-frontends-gigs-app.js",
-  },
-  URL: {
-    BASE: "http://localhost:3000",
-  },
 };
