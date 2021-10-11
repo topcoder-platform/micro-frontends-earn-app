b93b42942eab1fba51b31a6e922f58fff41cda84
LieutenantRoger <gair_2015@hotmail.com>
2021-10-11 15:09:20 +0800

    clean up

--- config/default.js
+++ config/default.js
@@ -6,8 +6,5 @@ module.exports = {
       "https://platform.topcoder-dev.com/challenges-app/topcoder-micro-frontends-challenges-app.js",
     "@topcoder/micro-frontends-gigs-app":
       "https://platform.topcoder-dev.com/gigs-app/topcoder-micro-frontends-gigs-app.js",
-  },
-  URL: {
-    BASE: process.env.URL_BASE || "https://www.topcoder-dev.com",
-  },
+  }
 };
