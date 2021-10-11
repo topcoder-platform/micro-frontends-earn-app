382e33203a228ea3aa13d6a30e48daaedcfdf3ae
LieutenantRoger <gair_2015@hotmail.com>
2021-10-12 00:51:45 +0800

    Hot fix disable gigs (#193)
    * remove path
    
    * restore url
    
    * directo to gigs

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,8 @@ module.exports = {
   MFE_CONFIG: {
     '@topcoder/micro-frontends-challenges-app': 'https://platform.topcoder.com/challenges-app/topcoder-micro-frontends-challenges-app.js',
     '@topcoder/micro-frontends-gigs-app': 'https://platform.topcoder.com/gigs-app/topcoder-micro-frontends-gigs-app.js',
+  },
+  URL: {
+    BASE: "https://www.topcoder.com",
   }
 };
