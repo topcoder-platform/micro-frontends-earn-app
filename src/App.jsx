7caeb5560e1c486673207fd933e82cbc9b6d1005
LieutenantRoger <gair_2015@hotmail.com>
2021-10-26 22:29:41 +0800

    output from challenge:30215305

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,11 @@ const App = () => {
           }
         />
         <Parcel
+          path="/earn/gigs/:externalId"
+          view="gig-details"
+          config={() => System.import("@topcoder/micro-frontends-gigs-app")}
+        />
+        <Parcel
           path="/earn/gigs"
           view="gigs"
           config={() => System.import("@topcoder/micro-frontends-gigs-app")}
