e3c0f4b6687cc5b9342d30539b6bd278cb209510
LieutenantRoger <gair_2015@hotmail.com>
2021-12-03 22:43:31 +0800

    refine

--- config/dev.js
+++ config/dev.js
@@ -1,12 +1,3 @@
 module.exports = {
-    MFE_CONFIG: {
-      "@topcoder/micro-frontends-challenges-app":
-        "http://localhost:8009/challenges-app/topcoder-micro-frontends-challenges-app.js",
-      "@topcoder/micro-frontends-gigs-app":
-        "https://platform.topcoder-dev.com/gigs-app/topcoder-micro-frontends-gigs-app.js",
-    },
-    URL: {
-      BASE: "http://localhost:3000",
-    },
-  };
+};
   
