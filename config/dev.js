7938121543033cb6ff2d3d21c8fa066e29755a5e
LieutenantRoger <gair_2015@hotmail.com>
2021-12-03 22:42:37 +0800

    optimize

--- config/dev.js
+++ config/dev.js
@@ -1,2 +1,12 @@
 module.exports = {
-};
+    MFE_CONFIG: {
+      "@topcoder/micro-frontends-challenges-app":
+        "http://localhost:8009/challenges-app/topcoder-micro-frontends-challenges-app.js",
+      "@topcoder/micro-frontends-gigs-app":
+        "https://platform.topcoder-dev.com/gigs-app/topcoder-micro-frontends-gigs-app.js",
+    },
+    URL: {
+      BASE: "http://localhost:3000",
+    },
+  };
+  
